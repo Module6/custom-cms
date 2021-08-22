42a076d01c7e607c08830ebcdec80e1d0175c00f
Bianca Romero <bromero@lesley.edu>
2021-08-22 19:57:47 -0400

    implemented database, unknown error on browser (?)

---
+++ media.php
@@ -0,0 +1,37 @@
+<?php
+include 'header.php';
+include 'nav.php';
+include 'db_connect.php';
+?>
+
+
+<html>
+
+<title>
+Custom CMS
+</title>
+
+
+<body>
+
+    <main>
+        <div>
+            <h2> Explaination of Work. </h2>
+            <p> My body of work plays on femininity, emotion, and color-provoked feelings. </p>
+         </div>
+
+         <img src="fem.jpg" alt="Woman Anatomy" width="200" height="300">
+         <img src="j.jpg" alt="Sunset" width="300" height="200">
+         <img src="me.jpg" alt="Bianca" width="250" height="300">
+         <img src="alone.jpg" alt="Still Life" width="300" height="200">
+         <img src="eye.jpeg" alt="Third Eye" width="200" height="300">
+    </main>
+
+
+
+<footer>
+    <p><h4> by Bianca Romero - 2021</h4></p>
+</footer>
+
+</body>
+</html>
